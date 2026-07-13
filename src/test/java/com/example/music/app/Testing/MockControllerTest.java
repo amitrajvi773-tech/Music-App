9b7f2d84e44f520d5241199ad155fa8ae7c7c581
amit pratap singh <amitrajvi773@gmail.com>
2026-07-14 00:22:09 +0530

    update

--- src/test/java/com/example/music/app/Testing/MockControllerTest.java
+++ src/test/java/com/example/music/app/Testing/MockControllerTest.java
@@ -1,8 +1,17 @@
 package com.example.music.app.Testing;
 
+import com.example.music.app.Controller.SongController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
+@WebMvcTest(SongController.class)
 public class MockControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SongService songService;
+
+    @MockBean
+    private SongRepository songRepository;
 
 }
